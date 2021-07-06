51051b00af18a1c6dc8329198e548936a7ff11d1
kimnakyung <35446851+dd6679@users.noreply.github.com>
2021-07-06 22:14:11 +0900

    2019 카카오 개발자 겨울 인턴십_크레인 인형뽑기 게임

---
+++ Programmers/CraneClawMachineGame.py
@@ -0,0 +1,17 @@
+def solution(board, moves):
+    basket = []
+    result = 0
+    
+    for i in moves:
+        #크레인 작동위치 i-1
+        i = i - 1
+        for j in range(len(board)):
+            if board[j][i] > 0:
+                basket.append(board[j][i])
+                board[j][i] = 0
+                if len(basket) > 1:
+                    if basket[-1] == basket[-2]:
+                        result += 1
+                        basket = basket[:-2]
+                break
+    return result*2

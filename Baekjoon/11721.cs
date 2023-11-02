0e79eaeb92611e9ebff281268e77aa7fe196bc9d
kimnakyung <35446851+dd6679@users.noreply.github.com>
2023-11-02 20:28:05 +0900

    Create 11721.cs
    열 개씩 끊어 출력하기

---
+++ Baekjoon/11721.cs
@@ -0,0 +1,29 @@
+using System;
+namespace PS_Repository_CSharp
+{
+    public class _1721_TenPrintOut
+    {
+        static void Main(String[] args)
+        {
+            string input = Console.ReadLine();
+            int len = input.Length;
+            int i = 0;
+            // 0 ~ 9, 10 ~ 19
+            // 10으로 나눈 줄 수 만큼 반복
+            while(i <= len / 10)
+            {
+                string input_1 = input;
+
+                if ((i + 1) * 10 - 1 >= len)
+                {
+                    Console.WriteLine(input_1.Substring(i * 10, len - (i * 10)));
+                }
+                else
+                {
+                    Console.WriteLine(input_1.Substring(i * 10, 10));
+                }
+                i++;
+            }
+        }
+    }
+}
